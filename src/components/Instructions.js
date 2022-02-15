6cea1463545f16a0a45dd874aa2672f5c1dddab6
Viet <vietnboi@gmail.com>
2022-02-14 23:55:03 -0600

    updated breakpoints

--- src/components/Instructions.js
+++ src/components/Instructions.js
@@ -5,6 +5,8 @@ const Instructions = () => {
   return (
     <div className="instructions">
       <h2>Instructions</h2>
+      <h3 id="landscape">(ok i hope u have ur shit sideways now)</h3>
+      <h3 id="portrait">(plz i hope this means ur in portrait mode)</h3>
       <p>The animals will shuffle after each click.</p>
       <p>The score resets to 0 if an animal is selected more than once.</p>
       <p>
